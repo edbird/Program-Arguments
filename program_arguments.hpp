e5bb6dfe42a1130e9e9ccfbee0e23d7d9e812067
ecb <ecb@hostname.hep.manchester.ac.uk>
2018-04-30 15:09:58 +0100

    program_arguments.hpp initial version working

--- program_arguments.hpp
+++ program_arguments.hpp
@@ -2,39 +2,60 @@
 #define PROGRAMARGUMENTS_HPP
 
 
+#include <memory>
 #include <iostream>
 #include <sstream>
 #include <map>
 
 
+// TODO: do not need to store the argument NAME
+// only need to store the TRIGGER
+
+
 class ProgramArgument
 {
 
+    public:
+
     ProgramArgument(const std::string& name, const std::string& default_string)
-        : _name_{name}
-        , _default_string_{default_string}
+        //: _name_{name}
+        /*,*/: _default_string_{default_string}
         , _value_string_{default_string}
     {
     }
 
-    void Set(const std::string& value_string)
+    void SetValue(const std::string& value_string)
     {
         _value_string_ = value_string;
     }
 
-    std::string Get() const
+    std::string GetValue() const
     {
         return _value_string_;
     }
 
+    /*
+    std::string GetName() const
+    {
+        return _name_;
+    }
+    */
+
+    std::string GetDefaultValue() const
+    {
+        return _default_string_;
+    }
+
     private:
 
-    std::string _name_;
+    //std::string _name_;
     std::string _default_string_;
     std::string _value_string_;
 
 };
 
+
+// TODO: multiple triggers
 class ProgramArguments
 {
 
@@ -42,68 +63,168 @@ class ProgramArguments
 
     public:
 
-    void Process(int argc, char* argv[], std::ofstream& error_stream = std::cerr)
+    std::string Get(const std::string& name, std::ostream& error_stream = std::cerr) const
+    {
+        // search for name
+        NameConstIterator_t name_it{_name_map_.find(name)};
+        if(name_it != _name_map_.end())
+        {
+            
+            // name found
+            return name_it->second->GetValue();
+
+        }
+        else
+        {
+            error_stream << error_name_not_found(name) << std::endl;
+            throw "error name not found";
+        }
+    }
+
+    std::string GetDefault(const std::string& name, std::ostream& error_stream = std::cerr) const
+    {
+        
+        // search for name
+        NameConstIterator_t name_it{_name_map_.find(name)};
+        if(name_it != _name_map_.end())
+        {
+            
+            // name found
+            return name_it->second->GetDefaultValue();
+
+        }
+        else
+        {
+            error_stream << error_name_not_found(name) << std::endl;
+            throw "error name not found";
+        }
+    }
+
+    void Process(int argc, char* argv[], std::ostream& error_stream = std::cerr)
     {
 
         for(int ix{1}; ix < argc; ++ ix)
         {
 
             // current argument
-            std::string arg(argv[ix]);
+            std::string trigger(argv[ix]);
 
-            ArgumentMapIterator_t it{_argument_map_.find(arg)};
-            if(it != _argument_map_.end())
+            // search for trigger
+            TriggerIterator_t trigger_it{_trigger_map_.find(trigger)};
+            if(trigger_it != _trigger_map_.end())
             {
+
+                // trigger found
                 if(++ ix < argc)
                 {
                     // current value corresponding to arg
                     std::string value(argv[ix]);
-                    it->second = value;
+                    trigger_it->second->SetValue(value);
                 }
                 else
                 {
-                    error_stream << error_message_no_argument_following_trigger() << std::endl;
+                    // no value following arg
+                    // TODO: what if value is missing but another arg trigger is provided?
+                    // eg: --filename --parameter 0.5
+                    // the filename following --filename is missing
+                    error_stream << error_no_argument_following_trigger(trigger, "NAME", "THE_TYPE", "FOLLOW_ARG") << std::endl;
                 }
 
             }
             else
             {
-                error_stream << error_message_arg_trigger_not_found(arg, ix) << std::endl;
+                // trigger not found
+                error_stream << error_arg_trigger_not_found(trigger, ix) << std::endl;
             }
 
         }
 
     }
 
-    void Add(const std::string& arg_trigger, const std::string& arg_name, const std::string& arg_defaut_string, std::ofstream& error_stream = std::cerr)
+    // TODO: this function cannot print the trigger following the name because it does not know which
+    // trigger goes with which name?
+    void Print(std::ostream& output_stream = std::cout)
+    {
+        NameIterator_t it{_name_map_.begin()};
+        for(; it != _name_map_.end(); ++ it)
+        {
+            output_stream << it->first << " " << /*it->second.GetName() << " " <<*/ it->second->GetValue() << " " << it->second->GetDefaultValue() << std::endl;
+        }
+
+    }
+
+    // NOTE: arg_name isn't a "name" it is a reference or key
+    // NOTE: changed arg_name -> arg_key
+    void Add(const std::string& arg_name, const std::string& arg_trigger, const std::string& arg_default_string, std::ostream& error_stream = std::cerr)
     {
 
-        ArgumentMapIterator_t it{_argument_map_.find(arg)};
-        if(it == _argument_map_.end())
+        // check name and trigger are not used
+
+        NameIterator_t it_name{_name_map_.find(arg_name)};
+        if(it_name != _name_map_.end())
         {
-            ProgramArgument new_program_argument(arg_name, arg_default_string);
-            _argument_map_[arg_trigger] = new_program_argument;
+            // error: name exists
+            error_stream << error_name_exists(arg_name) << std::endl;
+            throw "error name exists";
         }
-        else
+
+        TriggerIterator_t it_trigger{_trigger_map_.find(arg_trigger)};
+        if(it_trigger != _trigger_map_.end())
         {
-            error_stream << error();
+            // error: trigger exists
+            error_stream << error_trigger_exists(arg_trigger) << std::endl;
+            throw "error trigger exists";
         }
+
+        std::shared_ptr<ProgramArgument> program_argument_p(new ProgramArgument(arg_name, arg_default_string));
+        _trigger_map_[arg_trigger] = program_argument_p;
+        _name_map_[arg_name] = program_argument_p;
+
     }
 
     private:
 
-    std::string error_message_no_argument_arg_namewing_trigger(const std::string& arg_trigger, const std::string& arg_name, const std::string arg_type) const
+
+    std::string error_no_argument_following_trigger(const std::string& arg_trigger, const std::string& arg_name, const std::string& arg_type, const std::string& follow_arg) const
     {
         std::stringstream ret;
         ret << "[ ERROR ] : " << arg_trigger << " " << arg_name << std::endl;
-        ret << "[       ] : " << expected " " << arg_type << " " << follow_arg << " following argument " << arg_trigger << std::endl;
+        ret << "[       ] : " << "expected " << arg_type << " " << follow_arg << " following argument " << arg_trigger << std::endl;
         return ret.str();
     }
 
-    std::string error_message_arg_trigger_not_found(const std::string& arg, const int index) const
+
+    std::string error_arg_trigger_not_found(const std::string& arg, const int index) const
     {
         std::stringstream ret;
         ret << "[ ERROR ] : unrecognized argument in ProgramArguments::Process(): " << arg << " at index " << index << std::endl;
+        return ret.str();
+    }
+
+
+    std::string error_name_exists(const std::string& name) const
+    {
+        std::stringstream ret;
+        ret << "[ error ] : name " << name << " exists in map" << std::endl;
+        ret << "          : ProgramArguments::Add()" << std::endl;
+        return ret.str();
+    }
+
+
+    std::string error_trigger_exists(const std::string& trigger) const
+    {
+        std::stringstream ret;
+        ret << "[ error ] : trigger " << trigger << " exists in map" << std::endl;
+        ret << "          : ProgramArguments::Add()" << std::endl;
+        return ret.str();
+    }
+
+
+    std::string error_name_not_found(const std::string& name) const
+    {
+        std::stringstream ret;
+        ret << "[ ERROR ] : name " << name << " not found" << std::endl;
+        ret << "          : ProgramArguments::Get()" << std::endl;
         return ret.str();
     }
 
@@ -113,8 +234,14 @@ class ProgramArguments
     // todo: at the moment sort this string using arg_trigger variable as the key
     // however should sort using the arg_name key for retrival later!
     // TODO: implement a new map
-    std::map<const std::string, ProgramArgument> _argument_map_;
-    typedef std::map<const std::string, ProgramArgument>::iterator ArgumentMapIterator_t;
+    std::map<const std::string, std::shared_ptr<ProgramArgument>> _name_map_;
+    std::map<const std::string, std::shared_ptr<ProgramArgument>> _trigger_map_;
+    typedef std::map<const std::string, std::shared_ptr<ProgramArgument>>::iterator NameIterator_t;
+    typedef std::map<const std::string, std::shared_ptr<ProgramArgument>>::iterator TriggerIterator_t;
+    typedef std::map<const std::string, std::shared_ptr<ProgramArgument>>::const_iterator NameConstIterator_t;
+    typedef std::map<const std::string, std::shared_ptr<ProgramArgument>>::const_iterator TriggerConstIterator_t;
+
+    // TODO: implement as shared_ptr to allocated object
 
 };
 
